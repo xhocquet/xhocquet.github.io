baaf49ee9a06fef16bb1824bfce86d0865269cf8
xhocquet <xhocquet@gmail.com>
2025-09-27 15:39:23 -0400

    Wipe files

--- japanese/japanese.js
+++
@@ -1,5 +0,0 @@
-var toggleDropdown = function(event) {
-  var siblings = event.currentTarget.parentElement.children;
-  var content = siblings[siblings.length-1];
-  content.classList.toggle('show');
-}
